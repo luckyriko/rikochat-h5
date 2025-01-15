0a438a67a15b01f9e0371a27a04253b47249d956
lcukyriko <luckyriko@qq.com>
2025-01-15 23:51:32 +0800

    feat: chat web

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <!-- 使用 type="home" 属性设置首页，其他页面不需要设置，默认为page；推荐使用json5，更强大，且允许注释 -->
-<route lang="json5" type="home">
+<route lang="json5">
 {
   style: {
     navigationStyle: 'custom',
@@ -35,15 +35,13 @@ import { TestEnum } from '@/typings'
 import PLATFORM from '@/utils/platform'
 
 defineOptions({
-  name: 'Home',
+  name: 'Index',
 })
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 const author = ref('菲鸽')
-const description = ref(
-  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
-)
+const description = ref(' a deepseek chat app')
 // 测试 uni API 自动引入
 onLoad(() => {
   console.log(author)
